9259d267d7bb146465fa44c3b80fb4f5005917d7
gunnarsireus <gunnar.sireus@gmail.com>
2016-08-12 15:04:47 +0200

    Add project files.

---
+++ BuckyRoberts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BuckyRoberts.MvcApplication" Language="C#" %>
---
+++ BuckyRoberts/scripts/Index.js
@@ -0,0 +1,2 @@
+var Bacon = React.createClass({});
+ReactDOM.render(<h2>Hej jag heter Gunnar</h2>, document.getElementById('example'));
